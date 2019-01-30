a46cc69cba9e66cc71fc02ee58fdee67d96a1202
Macbook <macbook@nikolay-macbook.local>
2019-01-30 23:34:50 +0200

    DZL3 + 1.2

--- DZL3/ViewController.swift
+++ DZL3/ViewController.swift
@@ -16,11 +16,11 @@ class ViewController: UIViewController {
         
         maxMumber(numberOne: 2, numberTow: 2)
         squareAndCubeOfNumber(number: 2)
-        numberOrder()
+        numberOrder(givenNumber: 5)
         
     }
 
-    func maxMumber(numberOne: Int, numberTow: Int) {
+    func maxMumber (numberOne: Int, numberTow: Int) {
         if numberOne > numberTow {
             print("Max number is \(numberOne)")
             return
@@ -32,16 +32,22 @@ class ViewController: UIViewController {
             print("Numers are equal ")
     }
     
-    func squareAndCubeOfNumber(number: Int) {
+    func squareAndCubeOfNumber (number: Int) {
         let squareNumber = number * number
         print("Square of number \(number) is \(squareNumber)")
         let cubeNumber = squareNumber * number
         print("Cube of number \(number) is \(cubeNumber)")
     }
     
-    func  numberOrder() {
-        let numberOrder = 0...3
-        print("Number Order \(numberOrder)")
+    func  numberOrder (givenNumber: Int) {
+        print("Function numberOrder is Work ->")
+        for i in 0...givenNumber {
+            print(i)
+        }
+        for i in (-givenNumber)...0 {
+            print(-i)
+        }
+        print("Function numberOrder is Work <-")
     }
 }
 
