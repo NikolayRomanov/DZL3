f2e446c10fb67340f88e7adf522b2db5e78885f4
Macbook <macbook@nikolay-macbook.local>
2019-01-30 23:14:37 +0200

    DZL3 1.0, 1.1

--- DZL3/ViewController.swift
+++ DZL3/ViewController.swift
@@ -13,8 +13,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        maxMumber(numberOne: 2, numberTow: 2)
+        squareAndCubeOfNumber(number: 2)
+        numberOrder()
+        
     }
 
-
+    func maxMumber(numberOne: Int, numberTow: Int) {
+        if numberOne > numberTow {
+            print("Max number is \(numberOne)")
+            return
+        }
+        if numberOne < numberTow {
+            print("Max number is \(numberTow)")
+            return
+        }
+            print("Numers are equal ")
+    }
+    
+    func squareAndCubeOfNumber(number: Int) {
+        let squareNumber = number * number
+        print("Square of number \(number) is \(squareNumber)")
+        let cubeNumber = squareNumber * number
+        print("Cube of number \(number) is \(cubeNumber)")
+    }
+    
+    func  numberOrder() {
+        let numberOrder = 0...3
+        print("Number Order \(numberOrder)")
+    }
 }
 
