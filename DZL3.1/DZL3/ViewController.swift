c9323f79d7dda4dc8754e671cd9fb01279ad71b7
Macbook <macbook@nikolay-macbook.local>
2019-02-12 13:33:42 +0200

    iiii

--- DZL3.1/DZL3/ViewController.swift
+++ DZL3.1/DZL3/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //sd
+        //sdghfchgfcgfchvhg
         maxMumber(numberOne: 2, numberTow: 2)
         squareAndCubeOfNumber(number: 2)
         numberOrder(givenNumber: 5)
