aab5b58729478102ea13724c46576121d24948a3
Macbook <macbook@nikolay-macbook.local>
2019-02-12 13:29:42 +0200

    DZL4.1 add coment

--- DZL3.1/DZL3/ViewController.swift
+++ DZL3.1/DZL3/ViewController.swift
@@ -13,14 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        //sd
         maxMumber(numberOne: 2, numberTow: 2)
         squareAndCubeOfNumber(number: 2)
         numberOrder(givenNumber: 5)
         countDivisor(number: 8128)
     }
 
-    func maxMumber (numberOne: Int, numberTow: Int) {
+    func maxMumber(numberOne: Int, numberTow: Int) {
         if numberOne > numberTow {
             print("Max number is \(numberOne)")
             return
@@ -32,14 +32,14 @@ class ViewController: UIViewController {
             print("Numers are equal ")
     }
     
-    func squareAndCubeOfNumber (number: Int) {
+    func squareAndCubeOfNumber(number: Int) {
         let squareNumber = number * number
         print("Square of number \(number) is \(squareNumber)")
         let cubeNumber = squareNumber * number
         print("Cube of number \(number) is \(cubeNumber)")
     }
     
-    func  numberOrder (givenNumber: Int) {
+    func  numberOrder(givenNumber: Int) {
         print("Function numberOrder is Work ->")
         for i in 0...givenNumber {
             print(i)
@@ -50,7 +50,7 @@ class ViewController: UIViewController {
         print("Function numberOrder is Work <-")
     }
     
-    func countDivisor (number: Int) {
+    func countDivisor(number: Int) {
         var countDivisor = 0
         var divisior = 0
         for i in 1 ..< number {
@@ -69,5 +69,6 @@ class ViewController: UIViewController {
             print("Number \(number) is NOT perfect")
         }
     }
+    
 }
 
